dffa0f69410aa2015dc5ebcf9807c69e7e9c54e6
Wktb <113788425+Wktb@users.noreply.github.com>
2022-09-18 01:46:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -108,7 +108,7 @@ export const config = {
         {"keyword": "marry_day", date: "2022-08-05"},
       ]
     },
-    {
+ {
       name: "老婆2",
       id: "",
       useTemplateId: "",
@@ -117,7 +117,7 @@ export const config = {
       horoscopeDate: '',
       horoscopeDateType: '',
       openUrl: "https://wangxinleo.cn",
-      festivals: [ ],
+      festivals: [],
       customizedDateList: []
     },
     {
@@ -141,10 +141,10 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-   CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-   CALLBACK_USERS: [
+  CALLBACK_USERS: [
     {
       // 一般都填自己
       name: "自己",
